fb75c86b5f72b7030c92fe3c6bd2ca361b93f680
LesleyBonyo <lesleybonyo@gmail.com>
2023-05-26 22:42:01 +0300

    List group

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,8 +1,13 @@
 //import { Fragment } from "react";
+//import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Tokyo", "New York", "Kenya", "Paris", "London"];
-
+  let selectedIndex = 0;
+  //hook
+  useState;
   //const message = items.length === 0 ? <p>No item found</p> : null;
 
   const getMessage = () => {
@@ -16,6 +21,8 @@ function ListGroup() {
       </>
     );
     */
+  //event handler
+  //const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
@@ -23,9 +30,15 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={() => {
+              selectedIndex = index;
+            }}
           >
             {item}
           </li>
